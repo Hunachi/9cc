a3245481fce5769a5d132cdedaeca4b511e0529d
Hunachi <hanahanahunachi@gmail.com>
2020-10-31 14:51:18 +0900

    加減算のできるコンパイラの作成

--- test.sh
+++ test.sh
@@ -18,5 +18,7 @@ assert() {
 
 assert 0 0
 assert 42 42
+assert 21 "5+20-4"
+assert 27 "28-2+1"
 
 echo OK
--- tmp.s
+++ tmp.s
@@ -1,5 +1,7 @@
 .intel_syntax noprefix
 .globl main
 main:
-  mov rax, 42
+  mov rax, 28
+  sub rax, 2
+  add rax, 1
   ret
